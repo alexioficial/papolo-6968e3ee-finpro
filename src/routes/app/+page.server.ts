0416f46f03922a2e1a20dfeeae4a057cf11b38b2
Papolo <papolo@bot.local>
2026-05-31 19:18:17 +0000

    test: add Transaction model import

--- src/routes/app/+page.server.ts
+++ src/routes/app/+page.server.ts
@@ -1,8 +1,8 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-// Test which model import breaks
 import '../lib/server/models/Account';
+import '../lib/server/models/Transaction';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	if (!locals.user) throw error(401, 'No autorizado');
